c5ab704b330a5e22e4abe76429a9b7a441a13cc2
iyume <iyumelive@gmail.com>
2021-04-28 12:33:01 +0800

    improve recent send msg

--- handlers/recent.py
+++ handlers/recent.py
@@ -15,11 +15,23 @@ async def recent_handler(bot: Bot, event: MessageEvent, state: T_State) -> Any:
         if not current_user.code:
             raise ValueError
         userinfo = await api.query.userinfo(current_user.code, with_recent=True)
+        recent = userinfo.recent_score
+        if not recent:
+            raise RuntimeError
         if current_user.recent_type == 'text':
             await arc.send('\n'.join((
                 f"name: {userinfo.name}",
-                f"rating: {userinfo.rating}",
-                f"最近游玩: {userinfo.recent_score}"
+                f"PTT: {userinfo.rating}",
+                f'最近游玩: {recent.song_id}',
+                f'难度: {recent.difficulty}',
+                f'Score: {recent.score}',
+                f'PTT: {recent.rating:.2f}',
+                f'大P: {recent.shiny_perfect_count}',
+                f'小P: {recent.perfect_count - recent.shiny_perfect_count}',
+                f'count P: {recent.perfect_count}',
+                f'count FAR: {recent.near_count}',
+                f'count MISS: {recent.miss_count}',
+                f'Time: {recent.time_played}'
             )))
         else:
             await arc.send('this_is_recent.jpg')
